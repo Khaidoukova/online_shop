1623a754b2bf1f24a298d6c457b3eb7a2f6bc6f8
Olga Khaidoukova <khaidoukova@inbox.ru>
2023-08-02 17:14:28 +0500

    HW 20.1 (6) minor corrections in commands.py

--- catalog/management/commands/commands.py
+++ catalog/management/commands/commands.py
@@ -1,4 +1,6 @@
 from django.core.management import BaseCommand
+from django.db import connection
+
 from catalog.models import Category
 
 
@@ -8,6 +10,10 @@ class Command(BaseCommand):
         data = [{'name': 'Бакалея', 'description': 'список товаров в этой категории'},
                  {'name': 'Выпечка', 'description': 'список свежей выпечки'},
                  {'name': 'Готовые блюда', 'description': 'Список свежих готовых блюд'},]
+
+        with connection.cursor() as cursor:
+            cursor.execute("ALTER SEQUENCE catalog_product_id_seq RESTART WITH 1")
+            cursor.execute("ALTER SEQUENCE catalog_category_id_seq RESTART WITH 1")
 
         new_data = []
         for item in data:
